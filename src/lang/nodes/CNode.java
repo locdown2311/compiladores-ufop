e20bfaa777e6d89fcec622eafdd46600967d0e0d
Igor Carvalho <igor2396>
2024-09-30 10:19:41 -0300

    TEMP: Removido

--- src/lang/nodes/CNode.java
+++ src/lang/nodes/CNode.java
@@ -3,21 +3,21 @@ package lang.nodes;
 import lang.nodes.dotutils.DotFile;
 import lang.nodes.environment.Env;
 
-public abstract class CNode  extends beaver.Symbol  {
+public abstract class CNode extends  {
 
-   public CNode(){
-        super();
-   }
+     public CNode() {
+     }
 
-   public int getLine(){
-        return super.getLine(getStart());
-   }
+     public int getLine() {
+          return -1;
+     }
 
-   public int getColumn(){
-        return super.getColumn(getStart());
-   }
+     public int getColumn() {
+          return -1;
+     }
 
-   abstract public int toDot(DotFile d);
-   abstract public void interp(Env e);
+     abstract public int toDot(DotFile d);
+
+     abstract public void interp(Env e);
 
 }
